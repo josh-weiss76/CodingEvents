4dfbbb0b3613b3ff2ad2d025c381cbac316fe940
josh-weiss76 <59374753+josh-weiss76@users.noreply.github.com>
2020-06-23 13:25:51 -0400

    Add project files.

---
+++ CodingEvents/Controllers/EventsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace CodingEvents.Controllers
+{
+    public class EventsController : Controller
+    {
+        static private List<string> Events = new List<string>();
+
+        //GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //Events.Add("Retirement Ceremony");
+            //Events.Add("Last Day of LaunchCode Code Camp");
+            //Events.Add("First Day at Microsoft");
+            ViewBag.events = Events;
+            return View();
+        }
+        //Adding action method to add a form to update the list of events
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        //Adding action method to handle form submission
+        [HttpPost]
+        [Route("/Events/Add")]  
+        public IActionResult NewEvent(string name)
+        {
+            Events.Add(name);
+            return Redirect("/Events");
+        }
+    }
+}
