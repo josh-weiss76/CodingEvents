a942b6b47ff9893cf3540c158afc449a9725210c
josh-weiss76 <59374753+josh-weiss76@users.noreply.github.com>
2020-06-24 15:14:05 -0400

    Added Model code, the ability to delete events, and modify events

--- CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/Controllers/EventsController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodingEvents.Data;
+using CodingEvents.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 
@@ -9,13 +11,11 @@ namespace CodingEvents.Controllers
 {
     public class EventsController : Controller
     {
-        static private Dictionary<string,string > Events = new Dictionary<string, string>();
-
         //GET: /<controller>/
         [HttpGet]
         public IActionResult Index()
         {
-            ViewBag.events = Events;
+            ViewBag.events = EventData.GetAll();
             return View();
         }
         //Adding action method to add a form to update the list of events
@@ -28,9 +28,45 @@ namespace CodingEvents.Controllers
         //Adding action method to handle form submission
         [HttpPost]
         [Route("/Events/Add")]
-        public IActionResult NewEvent(string name, string description)
+        public IActionResult NewEvent(Event newEvent)
         {
-            Events.Add(name, description);
+            EventData.Add(newEvent);
+            return Redirect("/Events");
+        }
+        
+        //Deleting action method to handle form removal of an event
+        public IActionResult Delete()
+        {
+            ViewBag.events = EventData.GetAll();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int[] eventIds)
+        {
+            foreach(int eventId in eventIds)
+            {
+                EventData.Remove(eventId);
+            }
+            return Redirect("/Events");
+        }
+
+        //Editing action method to edit fields submitted
+
+        public IActionResult Edit(int eventId)
+        {
+            ViewBag.eventToEdit = EventData.GetById(eventId);
+            ViewBag.Title = "Edit Event " + ViewBag.eventToEdit.Name + "(id = " + ViewBag.eventToEdit.Id + ")";
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Edit")]
+        public IActionResult SubmitEditEventForm(int eventId, string name, string description)
+        {
+            ViewBag.eventToEdit = EventData.GetById(eventId);
+            ViewBag.eventToEdit.Name = name;
+            ViewBag.eventToEdit.Description = description;
             return Redirect("/Events");
         }
     }
